8f755e3a4d1c22133ac6224a336fa2fdae04b936
Stefan Sollander <stesol01@users.noreply.github.com>
2014-10-03 07:01:41 +0200

    Update DrawGUI.java

--- DrawGUI.java
+++ DrawGUI.java
@@ -1,6 +1,27 @@
-
+import javax.swing.*;
 
 public class DrawgGUI {
+
+
+
+    public DrawGUI(){
+        
+        initGUI();
+    }
+
+
+    private void initGUI(){
+        
+        /*
+        *   Inställningar för JFrame objektet
+        *
+        */
+        this.frameDim = new Dimension(300,200);
+        
+        this.setTitle("DrawingGUI");
+        this.setSize( (int) frameDim.getWidth(), (int) frameDim.getHeight() );
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
 
 
     public static void main(String[] args ) {
